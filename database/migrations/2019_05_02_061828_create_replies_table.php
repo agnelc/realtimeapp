f6dfe631c375f4622635c8e078b886460ae39c05
AGNEL <agnel.prayers@gmail.com>
2019-05-07 16:06:36 +0530

    Relationships

--- database/migrations/2019_05_02_061828_create_replies_table.php
+++ database/migrations/2019_05_02_061828_create_replies_table.php
@@ -16,7 +16,7 @@ class CreateRepliesTable extends Migration
         Schema::create('replies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->text('body');
-            $table->integer('question_id')->unsigned();
+            $table->bigInteger('question_id')->unsigned();
             $table->integer('user_id');
             $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
