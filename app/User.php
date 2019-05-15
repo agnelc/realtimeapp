874e95ffba3db98592cc3c7a60342a11552d523e
AGNEL <agnel.prayers@gmail.com>
2019-05-15 17:29:16 +0530

    JWT Auth

--- app/User.php
+++ app/User.php
@@ -6,11 +6,9 @@ use App\Model\Question;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-//use Tymon\JWTAuth\Contracts\JWTSubject;
-//class User extends Authenticatable implements JWTSubject
-
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -52,23 +50,23 @@ class User extends Authenticatable
      *
      * @return mixed
      */
-//    public function getJWTIdentifier()
-//    {
-//        return $this->getKey();
-//    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
     /**
      * Return a key value array, containing any custom claims to be added to the JWT.
      *
      * @return array
      */
-//    public function getJWTCustomClaims()
-//    {
-//        return [];
-//    }
-//    public function setPasswordAttribute($value)
-//    {
-//        $this->attributes['password'] = bcrypt($value);
-//    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 
 
 }
